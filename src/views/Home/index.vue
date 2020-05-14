1b02733a7aef9c183e6dac7120c35a2cc9010d77
mapuaptu <mapuaptu@gmail.com>
2020-05-14 14:10:20 +0400

    Create base views, remove unused default files.

---
+++ src/views/Home/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div :class="$style.home">
+    HOME
+  </div>
+</template>
+
+<script src="./index.js"></script>
+<style src="./index.scss" lang="scss" module></style>
---
+++ src/views/Todo/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div :class="$style.todo">
+    TODO
+  </div>
+</template>
+
+<script src="./index.js"></script>
+<style src="./index.scss" lang="scss" module></style>
